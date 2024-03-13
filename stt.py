8da68522a0d2d852b9350f5cee1fc0e657a5fb36
Vegar Karlsen <vegar@live.no>
2024-03-13 13:54:36 +0100

    Fix some bugs.

--- stt.py
+++ stt.py
@@ -3,48 +3,34 @@ from STT.whisper_live.client import TranscriptionClient
 
 
 class STT:
-
     def __init__(self, debug) -> None:
         self.debug = debug
         self.client = TranscriptionClient(
-                "localhost",
-                9090,
-                is_multilingual=True,
-                # lang="en",
-                translate=False,
-                model="small"
-            )
+            "localhost",
+            9090,
+            is_multilingual=True,
+            # lang="en",
+            translate=False,
+            model="small",
+        )
         self.sim_queue = Queue()
 
+    # If debug is true, sim is started. Otherwize start main sst client
     def start(self):
         if self.debug:
             self.start_sim()
-        else:
-            # self.client = TranscriptionClient(
-            #     "localhost",
-            #     9090,
-            #     is_multilingual=True,
-            #     # lang="en",
-            #     translate=False,
-            #     model="small"
-            # )
-            self.client()
+            return
 
+        self.client()
 
     def get_prompt(self):
         if self.debug:
             return self.sim_queue.get()
-        
+
         return self.client.client.get_prompt()
-    
 
     def start_sim(self):
         while True:
-            self.sim_get_text()
-
-    def sim_get_text(self):
-        text_to_prompt = input("prompt: ")
-        self.sim_queue.put(text_to_prompt)
-
-
+            text_to_prompt = input("prompt: ")
+            self.sim_queue.put(text_to_prompt)
 
